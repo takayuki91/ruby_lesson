54ccb5cfc06c3b3931441d000609aabf52c8dcc3
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-25 14:30:12 +0000

    [Add]Test refactoring

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -4,22 +4,24 @@ require_relative "../lib/ticket"
 
 
 class GateTest < Minitest::Test
-  def test_gate
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
-
+  
+  # テストメソッドが実行される前にこのメソッドが毎回呼ばれる
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+  
+  def test_umeda_to_juso
     ticket = Ticket.new(160)
-    umeda.enter(ticket)
-    assert juso.exit(ticket) #まだ通っていない
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket) #まだ通っていない
   end
 
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
     ticket = Ticket.new(160)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
   end
 
 end
